e313eab1776cd36842076f0fac87817d5827124d
tomek <tom.sturo@gmail.com>
2026-01-29 18:10:03 +0200

    Remove build artifacts, add to gitignore

--- build/lib/debussy/config.py
+++
@@ -1,8 +0,0 @@
-"""Configuration for Debussy."""
-
-POLL_INTERVAL = 5
-YOLO_MODE = True
-SESSION_NAME = "debussy"
-
-PARALLEL_ROLES = ["developer", "tester", "reviewer"]
-SINGLETON_ROLES = ["integrator"]
